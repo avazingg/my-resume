71920ddef80afcaeffd03061501211ddddfd8006
Vasiliy <avazingg@ya.ru>
2022-07-20 14:45:15 +0300

    feat: add portfolio links

--- src/js/console.js
+++ src/js/console.js
@@ -11,13 +11,13 @@
 // }
 // let stroka = new Arcticle('helloStroka')
 // console.log(stroka.reverseString('strokahello'));
-console.log('strokahello');
-console.log('strokahello');
-function reverseArray(arr) {
-  let newArr = []
-  for (let i = 0; i < arr.length; i++) {
-    newArr.unshift(arr[i])
-  }
-  return newArr
-}
-console.log(reverseArray([1, 2, 3, 4, 5]))
+// console.log('strokahello');
+// console.log('strokahello');
+// function reverseArray(arr) {
+//   let newArr = []
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.unshift(arr[i])
+//   }
+//   return newArr
+// }
+// console.log(reverseArray([1, 2, 3, 4, 5]))
